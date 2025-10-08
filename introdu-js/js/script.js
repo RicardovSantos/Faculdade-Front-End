438a3f6f23befe2ef15bd964aa22030492c77669
Ricardo <ricardovieirads@outlook.com>
2025-10-07 21:00:31 -0300

    iniciando a js-funcoes

---
+++ introdu-js/js/script.js
@@ -0,0 +1,13 @@
+//primeira aula 1, dados numericos
+console.log(2);
+console.log(typeof 363.800);
+console.log(35*7);
+//strings
+console.log("texto impresso");
+console.log('texte em aspas simples');
+//2 strings
+console.log('texto de quebra de linha \n');
+console.log('texto com \t tabss');
+//concaternação
+console.log('ola '+' tudo bem'+'mal');
+console.log(`a soma è: ${2+2} `);
---
+++ js-estruturas/js/script.js
@@ -0,0 +1,16 @@
+// 1 variaveis
+//let nome = "João";
+//console.log(nome);
+let idade = 25;
+console.log(idade);
+let a=10, b=5, c=2;
+console.log(a, b, c);
+//função
+function soma(a, b) {
+    return a + b;
+}
+console.log(soma(5, 3));
+//prompt
+let nome=prompt("Digite seu nome:");
+alert("Olá, " + nome + "!");
+console.log("Nome digitado: " + nome);
